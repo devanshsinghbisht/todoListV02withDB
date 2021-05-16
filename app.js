3fcd1016ca08c380becf120f59b970a53a70780b
Devansh <Devanshsingh2222@gmail.com>
2021-05-16 14:39:24 +0530

    Add gitignore , procfilie and update ports

--- app.js
+++ app.js
@@ -132,8 +132,9 @@ app.get("/about", function (req, res) {
 });
 
 let port = process.env.PORT;
-if(port == null || port ==""){
-  port = 3000;
+
+if (port == null || port == "") {
+  port = 8000;
 }
 
 app.listen(port,function(){
